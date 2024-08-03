b9f15884298f7c5a3002364435e3c0a138a56ae6
emiliagit <94327724+emiliagit@users.noreply.github.com>
2024-08-03 12:43:05 -0300

    collectible

--- Assets/game/Scripts/Collectable/SceneManager.cs
+++ Assets/game/Scripts/Collectable/SceneManager.cs
@@ -20,10 +20,10 @@ public class SceneManager : MonoBehaviour
         contador++;
         recolectablesText.text = "Food collected: " + contador;
 
-        //if (contador == 10)
-        //{
-        //   SceneManager.LoadScene("Level1") ;
-        //}
+        if (contador == 10)
+        {
+            SceneManager.LoadScene("Level1");
+        }
     }
 
    
