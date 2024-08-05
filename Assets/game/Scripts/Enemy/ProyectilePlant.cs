aa0857927485446810b6f6f5409cdc763573593f
emiliagit <94327724+emiliagit@users.noreply.github.com>
2024-08-05 15:03:26 -0300

    enemy

---
+++ Assets/game/Scripts/Enemy/ProyectilePlant.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProyectilePlant : MonoBehaviour
+{
+
+    public Animator animator;
+
+    public float speed = 10f;
+
+
+    void Update()
+    {
+        // Mover el proyectil hacia adelante a una velocidad constante
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Colision con player");
+            animator.SetTrigger("Hit");
+            Destroy(gameObject,1f);
+        }
+        if (other.gameObject.TryGetComponent(out LifePlayer player))
+        {
+            player.TakeDamage(1);
+            //animator.SetTrigger("Hit");
+            Debug.Log("vida restada");
+        }
+    }
+}
